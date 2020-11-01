326902e52134658b9f3fb6075bdf16890c996ee3
I am the Carl <jamescarl96@gmail.com>
2020-11-01 01:41:15 -0500

    Bechmarks can produce flamegraphs.

--- native/common/benches/benchmarks.rs
+++ native/common/benches/benchmarks.rs
@@ -3,10 +3,9 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use tempfile::tempfile;
 
 fn iterate_fresh_file(c: &mut Criterion) {
-    let index = TerrainDiskStorage::initialize(tempfile().unwrap(), tempfile().unwrap()).unwrap();
-
     c.bench_function("iterate_fresh_file", |b| {
         b.iter(|| {
+            let index = TerrainDiskStorage::initialize(tempfile().unwrap(), tempfile().unwrap()).unwrap();
             index.get_chunks_in_range((-50, -50, -50), (50, 50, 50), |_chunk| Ok(())).unwrap();
         })
     });
@@ -16,11 +15,18 @@ fn iterate_pregen_file(c: &mut Criterion) {
     let index = TerrainDiskStorage::initialize(tempfile().unwrap(), tempfile().unwrap()).unwrap();
     index.get_chunks_in_range((-50, -50, -50), (50, 50, 50), |_chunk| Ok(())).unwrap();
 
+    let profiler = pprof::ProfilerGuard::new(100).unwrap();
+
     c.bench_function("iterate_pregen_file", |b| {
         b.iter(|| {
             index.get_chunks_in_range((-50, -50, -50), (50, 50, 50), |_chunk| Ok(())).unwrap();
         })
     });
+
+    if let Ok(report) = profiler.report().build() {
+        let file = std::fs::File::create("flamegraphs/iterate_pregen_file.svg").unwrap();
+        report.flamegraph(file).unwrap();
+    };
 }
 
 fn single_chunk_fresh_file(c: &mut Criterion) {
