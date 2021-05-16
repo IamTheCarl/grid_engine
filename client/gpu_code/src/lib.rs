80b2711a723c4f5dd52e794d0ce04a49410d4a54
I am the Carl <jamescarl96@gmail.com>
2021-05-16 12:22:11 -0400

    Prettier shader code.

--- client/gpu_code/src/lib.rs
+++ client/gpu_code/src/lib.rs
@@ -4,15 +4,15 @@
 #[cfg(not(target_arch = "spirv"))]
 use spirv_std::macros::spirv;
 
-use glam::{vec4, Vec3, Vec4};
+use glam::{Vec3, Vec4};
 
 #[spirv(vertex)]
 pub fn main_vs(a_position: Vec3, a_color: Vec3, v_color: &mut Vec3, #[spirv(position, invariant)] out_pos: &mut Vec4) {
     *v_color = a_color;
-    *out_pos = vec4(a_position.x, a_position.y, a_position.z, 1.0);
+    *out_pos = a_position.extend(1.0);
 }
 
 #[spirv(fragment)]
 pub fn main_fs(v_color: Vec3, f_color: &mut Vec4) {
-    *f_color = vec4(v_color.x, v_color.y, v_color.z, 1.0);
+    *f_color = v_color.extend(1.0);
 }
